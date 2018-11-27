f25e464a4ee71f9b39cb5a379084f6c6bbb14e20
dober <doberste8@gmail.com>
2018-11-27 06:19:01 +0000

    Test of /api routing

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  SERVER_API: '"http://disc-sports.bid:3000"',
-  // SERVER_API: '"http://mevn-template-server-doberste8.c9users.io"',
+  SERVER_API: '"http://localhost/api"',
+  // SERVER_API: '"http://disc-sports.bid:3000"',
+  // SERVER_API: '"http://mevn-template-server-dogit berste8.c9users.io"',
 }
